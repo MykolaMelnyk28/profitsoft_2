46cf62fdfd149c2c5298a36498fca734e7016c70
Mykola Melnyk <mykolamelnyk338@gmail.com>
2025-11-25 16:50:40 +0200

    Add H2 database for tests

---
+++ src/test/java/com/melnyk/profitsoft_2/config/TestConfig.java
@@ -0,0 +1,8 @@
+package com.melnyk.profitsoft_2.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Profile;
+
+@TestConfiguration
+@Profile("test")
+public class TestConfig { }
