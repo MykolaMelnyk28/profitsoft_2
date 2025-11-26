8a53e287207b98a9b48074f5172698e528908579
Mykola Melnyk <mykolamelnyk338@gmail.com>
2025-11-26 12:40:53 +0200

    Add Mapstruct to dependencies

---
+++ src/main/java/com/melnyk/profitsoft_2/mapper/BookMapper.java
@@ -0,0 +1,10 @@
+package com.melnyk.profitsoft_2.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BookMapper {
+
+
+
+}
---
+++ src/main/java/com/melnyk/profitsoft_2/mapper/GenreMapper.java
@@ -0,0 +1,8 @@
+package com.melnyk.profitsoft_2.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface GenreMapper {
+
+}
